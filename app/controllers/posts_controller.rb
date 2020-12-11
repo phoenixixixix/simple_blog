32e0d6205abd78f1c1fd97f0f23ab3ee684d65b0
barchuk oleg <barchukoleg12@gmail.com>
2020-12-11 21:43:14 +0200

    g posts controller + views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def post_params
+    params.require(:posts).permit(:title,
+                                  :text,
+                                  :category)
+  end
+end
