f60253bb6ed90707917984242927bb0080cecfb4
barchuk oleg <barchukoleg12@gmail.com>
2020-12-11 20:58:49 +0200

    add relation of Category and Post + migration AddCategoryToPost done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_175111) do
+ActiveRecord::Schema.define(version: 2020_12_11_183447) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2020_12_11_175111) do
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
+  add_foreign_key "posts", "categories"
 end
